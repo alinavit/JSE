e61adccf026eee4e63f1e08d70d29a405a578b29
alinavit <65871360+alinavit@users.noreply.github.com>
2024-06-13 13:20:01 +0200

    feat (extract): added justjoinit extraction

--- rundata.py
+++ rundata.py
@@ -1,8 +1,16 @@
 import logging.config
+import extractdata
+import config
 
-logging.config.fileConfig("C:\\Users\\48575\\PycharmProjects\\JSE4\\conf\\logging.conf")
+logging.config.fileConfig('conf/logging.conf')
 logger = logging.getLogger('RunData')
 
 logger.info('Job Start')
 
+just_join_it = extractdata.DataProcessingJJI(
+                    url_list=config.URLS_JUST_JOIN_IT,
+                    main_url=config.MAIN_JUST_JOIN_IT,
+                    source_name='justjoinit'
+                )
+just_join_it.run()
 
