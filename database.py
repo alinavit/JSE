95638cdcdc3362c7adab6cbb2f9f7d31ab4968ce
alinavit <65871360+alinavit@users.noreply.github.com>
2024-07-22 16:42:39 +0200

    feat: added download results and assessed_ids update

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import psycopg2
 import db_info
 import logging.config
@@ -7,7 +8,7 @@ logger = logging.getLogger('databaseLogger')
 
 
 class JSEDatabase:
-    def __init__(self, data, source):
+    def __init__(self, data=list(), source=''):
         self.data = data
         self.host = db_info.HOST
         self.database = db_info.DATABASE
@@ -47,7 +48,7 @@ class JSEDatabase:
             if isinstance(row, dict):
                 sql_statement = '''
                                 INSERT INTO 
-                                            positions_test(position_id,
+                                            positions(position_id,
                                             url,
                                             title,
                                             description,
@@ -106,3 +107,88 @@ class JSEDatabase:
                 logger.warning('Data provided unexpectedly. The data has to be a list of dictionaries.')
 
         self.conn.close()
+
+    def read(self, statement, columns=list()):
+        self.connect()
+        self.cur.execute(statement)
+        if len(columns) != 0:
+            df = pd.DataFrame(self.cur.fetchall(), columns=columns)
+        else:
+            df = pd.DataFrame(self.cur.fetchall())
+            col_names = {idx: desc[0] for idx, desc in enumerate(self.cur.description)}
+            df.rename(columns=col_names, inplace=True)
+        self.disconnect()
+        return df
+
+    def csv_download(self):
+        from datetime import date
+        import openpyxl
+        # download data from v_positions gold
+        df = self.read('''SELECT ass.value, pos.* 
+                          FROM v_positions_gold pos
+                          LEFT OUTER JOIN ASSESSED_IDS ass ON pos.position_id=ass.position_id''')
+        df.to_csv(f"files\data{date.today().strftime('%y_%m_%d')}.csv", index=False)
+
+
+    def populate_assessed_ids(self, csv_name):
+        self.connect()
+
+        create_temp_table = '''
+                            CREATE TEMP TABLE IF NOT EXISTS temp_assessed_ids (
+                            value NUMERIC, position_id NUMERIC, company_id NUMERIC, 
+                            title TEXT, description TEXT, key_words TEXT,
+                            k_words_desc TEXT, url TEXT, source_name TEXT, 
+                            category TEXT, salary TEXT, date_registered TEXT,
+                            type_of_work TEXT, experience TEXT, employment_type TEXT, 
+                            operating_mode TEXT, company_name TEXT
+                            )
+                            '''
+
+        copy_statement = f'''COPY temp_assessed_ids FROM 'C:/Users/48575/PycharmProjects/JSE4/files/{csv_name}.csv'
+                            DELIMITER ','  CSV HEADER; '''
+
+        insert_statement = '''INSERT INTO assessed_ids (value, position_id)
+                                SELECT value, position_id
+                                FROM temp_assessed_ids
+                                ON CONFLICT (position_id)
+                                DO UPDATE SET value = EXCLUDED.value;
+                              '''
+        try:
+            self.cur.execute(create_temp_table)
+            self.cur.execute(copy_statement)
+            self.cur.execute(insert_statement)
+            self.conn.commit()
+        except Exception as e:
+            logger.info('could not populate table assessed_ids')
+            logger.exception(f'exception: {e}')
+
+    def data_adjust(self):
+        self.connect()
+
+        sql_statement = '''
+            call public.latest_data_raw_bronze_to_silver()
+        '''
+
+        sql_statement2 = '''
+            call public.p_update_comp_ids_in_positions_silver()
+        '''
+
+        sql_collect_key_desc = '''
+            call p_collect_key_words()
+        '''
+
+        sql_select_unattractive_pos_ids = '''
+            call p_populate_un_attractive_positions() 
+        '''
+        try:
+            self.cur.execute(sql_collect_key_desc)
+            self.cur.execute(sql_statement)
+            self.cur.execute(sql_statement2)
+            self.cur.execute(sql_select_unattractive_pos_ids)
+
+            self.conn.commit()
+        except Exception as e:
+            print(f"Error: {e}")
+            self.conn.rollback()
+        finally:
+            self.disconnect()
